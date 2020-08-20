5176a9ec720747a6aae28e356f7216dfd557aedc
DelightedCat <bottledlactose@gmail.com>
2020-08-20 13:34:11 +0200

    Add activity placeholders and update Discord presence

--- main.py
+++ main.py
@@ -35,12 +35,21 @@ async def on_member_join(member):
 async def test(ctx):
     await ctx.send('Hello, world!')
 
-@tasks.loop(seconds=0.5)
+@tasks.loop(seconds=5.0)
 async def update_activity():
     global activity_index
+    await client.wait_until_ready()
 
-    activity = activities[activity_index]
-    print(activity['type'] + " " + activity['message'])
+    activity_data = activities[activity_index]
+
+    activity_type = discord.ActivityType[activity_data['type']]
+    activity_message = activity_data['message']
+
+    # Replace some template placeholders to create a dynamic string
+    activity_message = activity_message.replace('${guild_count}', str(len(client.guilds)))
+    activity_message = activity_message.replace('${user_count}', str(len(client.users)))
+
+    await client.change_presence(activity=discord.Activity(type=activity_type, name=activity_message))
 
     activity_index += 1
 
