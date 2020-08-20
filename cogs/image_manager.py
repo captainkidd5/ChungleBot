c42ad4c93979831c895fe74f6d926615f8c607cc
DelightedCat <bottledlactose@gmail.com>
2020-08-21 00:32:39 +0200

    Simplify some cog code

--- cogs/image_manager.py
+++ cogs/image_manager.py
@@ -1,27 +1,19 @@
-  
+import discord, json, random, asyncio
 from discord.ext import commands
-import discord
-import json
-import random
-import asyncio
 
 def setup(bot):
-        bot.add_cog(image_manager(bot))
+    bot.add_cog(image_manager(bot))
 
-#Eventually want to export token system to this file
+# Eventually want to export token system to this file
 
 class image_manager(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    
-
     @commands.command(name='grab',description='Summon Thanos to restore balance to a member in chat')
     async def thanos_command(self,ctx):
         await ctx.channel.send('Perfectly balanced...')
-        await ctx.channel.send(file=discord.File('images/thanos1.png'))   
-        await self.bot.wait_for('message',timeout=30)    
+        await ctx.channel.send(file=discord.File('images/thanos1.png'))
+        await self.bot.wait_for('message',timeout=30)
         await ctx.channel.send(file=discord.File('images/thanos2.png'))
         await ctx.channel.send(f'...as {ctx.author.name} should be.')
-        
-
--- cogs/math.py
+++ cogs/math.py
@@ -1,24 +1,16 @@
-  
+import discord, json, random, asyncio
 from discord.ext import commands
-import discord
-import json
-import random
-import asyncio
 
 def setup(bot):
-        bot.add_cog(math(bot))
+    bot.add_cog(math(bot))
 
-#Eventually want to export token system to this file
+# Eventually want to export token system to this file
 
 class math(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    
-
     @commands.command(description='For when you wanna settle the score some other way')
     async def choose(self, ctx,*choices : str):
         """Chooses between multiple choices."""
         await ctx.channel.send(random.choice(choices))
-        
-
