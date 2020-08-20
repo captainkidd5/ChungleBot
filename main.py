d26adde427a9e5ffc7cdbf0240a7c9699f7e4afa
DelightedCat <bottledlactose@gmail.com>
2020-08-20 13:05:09 +0200

    Add loading and updating of activities

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 import discord
+import json
 
-from discord.ext import commands
+from discord.ext import commands, tasks
 
 from os.path import join, dirname
 from dotenv import load_dotenv
@@ -11,6 +12,11 @@ load_dotenv(dotenv_path)
 
 TOKEN = os.environ.get("TOKEN")
 PREFIX = os.environ.get('PREFIX', '!chungle ')
+
+activity_index = 0
+
+with open(join(dirname(__file__), 'config', 'activities.json')) as json_file:
+    activities = json.load(json_file)
 
 client = commands.Bot(command_prefix=PREFIX)
 
@@ -29,4 +35,17 @@ async def on_member_join(member):
 async def test(ctx):
     await ctx.send('Hello, world!')
 
+@tasks.loop(seconds=0.5)
+async def update_activity():
+    global activity_index
+
+    activity = activities[activity_index]
+    print(activity['type'] + " " + activity['message'])
+
+    activity_index += 1
+
+    if activity_index >= len(activities):
+        activity_index = 0
+
+update_activity.start()
 client.run(TOKEN)
